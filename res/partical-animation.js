0f92054441a890fbfadd87fb8a6f2226f68b2b16
Andrew Hopper <andrewhopper@ya.ru>
2017-11-15 13:58:38 +0300

    Particles update

--- res/partical-animation.js
+++ res/partical-animation.js
@@ -3,7 +3,7 @@
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 100,
+      "value": 200,
       "density": {
         "enable": true,
         "value_area": 800
@@ -20,11 +20,6 @@ particlesJS("particles-js", {
       },
       "polygon": {
         "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
       }
     },
     "opacity": {
